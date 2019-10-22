c7dfc1a163ab6fa43c7d256d997529e403636945
Amos Chihi <appschihi@gmail.com>
2019-10-22 11:46:22 +0300

    Updated game.js

--- game.js
+++ game.js
@@ -12,6 +12,13 @@ ground.src = "images/ground.png";
 const foodImg = new Image();
 foodImg.src = "images/apple.png";
 
+// load audio files
+const dead = new Audio();
+const eat = new Audio();
+
+dead.src = "audio/death.ogg";
+eat.src = "audio/eat.ogg";
+
 // create snake
 let snake = [];
 
@@ -30,12 +37,31 @@ let food = {
 let score = 0;
 
 // control the snake 
+let d;
+
 document.addEventListener("keydown",direction);
 
 function direction(event){
-	
+	// alert(event);
+	if(event.keyCode == 37 && d != "RIGHT") {
+		d = "LEFT";
+	}else if(event.keyCode == 38 && d != "DOWN") {
+		d = "UP";
+	}else if(event.keyCode == 39 && d != "LEFT") {
+		d = "RIGHT";
+	}else if(event.keyCode == 40 && d != "UP") {
+		d = "DOWN";
+	}
 }
-
+// check collision
+function collision(head, array){
+	for(let i= 0; i<array.length; i++){
+		if(head.x == array[i].x && head.y == array[i].y) {
+			return true;
+		}
+	}
+	return false;
+}
 // draw everything to the canvas
 
 function draw(){
@@ -50,6 +76,50 @@ function draw(){
 	}
 
 	ctx.drawImage(foodImg, food.x, food.y);
+
+	// old head position
+	let snakeX = snake[0].x;
+	let snakeY = snake[0].y;
+
+	// which direction
+	if( d = "LEFT") snakeX -= box;
+	if( d = "UP") snakeY -= box;
+	if(d = "RIGHT") snakeX += box;
+	if(d = "DOWN") snakeY += box;
+
+	// If the snake eats the food
+	if(snakeX == food.x && snakeY == food.y){
+		score++;
+		eat.play();
+		food = {
+			x: Math.floor(Math.random()*17+1) * box,
+			y: Math.floor(Math.random()*15+3) * box
+		}
+		// don't remove the tail
+
+	}else{
+		//remove the tail
+		snake.pop();
+	}
+
+
+	// add new head
+	let newHead = {
+		x: snakeX,
+		y: snakeY
+	}
+
+
+	// game over
+	if(snakeX < box || snakeX > 17 * box || 
+		snakeY < 3 * box || snakeY > 17 * box ||
+		collision(newHead, snake)) {
+		clearInterval(game);
+		dead.play();
+	}
+
+
+	snake.unshift(newHead);
 
 	ctx.fillStyle = "white";
 	ctx.font = "45px Changa one";
